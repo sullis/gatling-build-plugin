890eabfea9ecc1d0a6d0c4f80be35338e397f6dd
Sébastien BREVET <sbrevet@gatling.io>
2021-02-02 15:37:10 +0100

    Self apply
    Motivation:
    This plugin will need to be release as other artifact.
    
    Modifications:
    Auto apply itself as GatlingOssPlugin
    
    Result:
    Autoformatting and ready to be plublished

--- src/main/scala/io/gatling/build/GatlingReleasePlugin.scala
+++ src/main/scala/io/gatling/build/GatlingReleasePlugin.scala
@@ -1,16 +1,29 @@
+/*
+ * Copyright 2011-2021 GatlingCorp (https://gatling.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.gatling.build
-
-import com.jsuereth.sbtpgp.PgpKeys.publishSigned
-
-import sbtrelease.ReleasePlugin.autoImport._
-
-import sbt.complete.DefaultParsers._
-import sbt.complete.Parser
 
 import io.gatling.build.release.GatlingReleaseProcess
 
-import sbt.Keys._
+import com.jsuereth.sbtpgp.PgpKeys.publishSigned
 import sbt._
+import sbt.Keys._
+import sbt.complete.DefaultParsers._
+import sbt.complete.Parser
+import sbtrelease.ReleasePlugin.autoImport._
 
 object GatlingReleasePlugin extends AutoPlugin {
   override def requires: Plugins = GatlingPublishPlugin
